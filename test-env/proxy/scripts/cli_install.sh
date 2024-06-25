a9fc5aad5de12eeac98116c60a0bb3dd7fb65d74
asanjabi <32405657+asanjabi@users.noreply.github.com>
2024-06-25 07:35:47 -0700

    added notes

--- test-env/proxy/scripts/cli_install.sh
+++ test-env/proxy/scripts/cli_install.sh
@@ -3,6 +3,8 @@ set -exuo pipefail
 source ~/.env
 source ~/additional_env
 
+# Install Azure CLI
+# use the step by step process just incase the script fails, it would be easier to debug
 # see https://learn.microsoft.com/en-us/cli/azure/install-azure-cli-linux?pivots=apt#option-2-step-by-step-installation-instructions
 # Get packages needed for installation process
 sudo apt-get update
@@ -27,6 +29,10 @@ sudo apt-get update
 sudo NEEDRESTART_MODE=a apt-get install azure-cli -y
 
 # Configure Azure CLI to use the proxy
+# Azure CLI uses it's own certificate bundle, so we need to append the proxy's certificate to it
+# This file is located under /opt/az/lib/<some version>/site-packages/certifi/cacert.pem
+# We will make a copy of this file and append the proxy's certificate to it
+# Then we will set the REQUESTS_CA_BUNDLE environment variable to point to the new certificate bundle
 
 # make a copy of the certificate bundle for Azure CLI
 find /opt/az/lib/*/site-packages/certifi -name cacert.pem -exec cp ""{}"" ~/ \;
@@ -36,8 +42,10 @@ cat $certfile_crt >> ~/cacert.pem
 echo export REQUESTS_CA_BUNDLE=~/cacert.pem >> ~/additional_env
 
 
+
 # Increase default timeout for pip installs since using proxy can slow down the process
 # some environments may need this to be set to a higher value
+# Also see https://pip.pypa.io/en/stable/topics/configuration/
 echo export PIP_DEFAULT_TIMEOUT=100 >> ~/additional_env
 
 
