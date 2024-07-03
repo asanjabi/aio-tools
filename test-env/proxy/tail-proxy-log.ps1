4134dd63fe26085a5e6523844b07b0cb84a12148
asanjabi <32405657+asanjabi@users.noreply.github.com>
2024-07-02 18:56:24 -0700

    add -NewWindow switch

--- test-env/proxy/tail-proxy-log.ps1
+++ test-env/proxy/tail-proxy-log.ps1
@@ -1,7 +1,15 @@
 using module "../../tools/pwsh/Tools.psm1"
+[CmdletBinding()]
+param (
+    [switch]$NewWindow)
+
+if ($NewWindow) {
+    Start-Process pwsh -ArgumentList "-NoExit", "-Command", "`$ErrorActionPreference = 'Stop'; . $PSCommandPath"
+    Remove-Module -name tools
+    exit
+}
 
 ReadVariablesFromFile ".env"
-
 
 $vmName = $proxy_VmName
 multipass exec $vmName -- sudo tail -f /var/log/squid/access.log
